92b660d5279276d6da61e0b1fafc2eee695006ab
HARISH SUTHAR <104978944+SUTHARHarish@users.noreply.github.com>
2025-05-21 21:20:21 +0530

    21-05-2025
    All the API-related code has been removed and a simple TSX template is ready.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,91 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Login from "./pages/authentication/pages/Login";
-import User from "./pages/Admin/User Master/User";
-import Configuration from "./pages/Admin/Configuration/Configuration";
-import ApiLog from "./pages/Admin/ApiLog/ApiLog";
-import IpWhiteList from "./pages/Admin/IPWhiteList/IpWhiteList";
-import Services from "./pages/Admin/Services/Services";
-import Recharge from "./pages/Admin/Recharge/Recharge";
-import Application from "./pages/Api Setup/Application/Application";
-import Subscribe from "./pages/Api Setup/Subscribe/Subscribe";
-import APIApproval from "./pages/Api Setup/API Approval/APIApproval";
-import KYCVerification from "./pages/KYC Verification/KYCVerification";
-import TodayReport from "./pages/Report/Today Report/TodayReport";
-import DayWiseReport from "./pages/Report/Day Wise Report/DayWiseReport";
-import ApprovedAPIReport from "./pages/Report/Approved API Report/ApprovedAPIReport";
-import RechargeReport from "./pages/Report/Recharge Report/RechargeReport";
 import ForgotPassword from "./pages/authentication/pages/ForgotPassword";
-import OtpVerification from "./pages/authentication/pages/OtpVerification";
-import Client from "./pages/Admin/Client/Client";
-import ChangePass from "./pages/Setting/Change Password/ChangePass";
-import Profile from "./pages/Setting/Profile/Profile";
-import { setGeoLocation } from "./utils/geoLocation";
-import { setIdentityData } from "./utils/common";
-import { toast, ToastContainer } from "react-toastify";
-import ApiService from "./pages/apiService/ApiService";
-import Setting from "./pages/Setting/Setting";
-import Admin from "./pages/Admin/Admin";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  // const isDark = useSelector((state: any) => state.layout.isDark);
-  const [isLocationGranted, setIsLocationGranted] = useState<boolean>(false); // this store Location Status (location is true and false)
-
-  useEffect(() => {
-    navigator.permissions
-      .query({ name: "geolocation" })
-      .then((PermissionStatus) => {
-        // Check Permission of Location
-        if (PermissionStatus.state === "denied") {
-          setIsLocationGranted(false);
-        } else {
-          setIsLocationGranted(true);
-          setGeoLocation();
-        }
-      });
-
-    // get Ip Address and set in local storange
-    setIdentityData();
-  }, []);
-
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/forgotpwd" element={<ForgotPassword />} />
-          <Route path="/optvarification" element={<OtpVerification />} />
-
           <Route path="/" element={<Layout />}>
             <Route path="dashboard" element={<Dashboard />} />
-            <Route path="dashboard/apiservice" element={<ApiService />} />
-            // Admin
-            <Route path="admin" element={<Admin/>} />
-            {/* <Route path="user" element={<User />} />
-            <Route path="client" element={<Client />} /> */}
-            <Route path="apilog" element={<ApiLog />} />
-            <Route path="Configuration" element={<Configuration />} />
-            <Route path="ipwhitelist" element={<IpWhiteList />} />
-            <Route path="services" element={<Services />} />
-            <Route path="recharge" element={<Recharge />} />
-            //Api Setup
-            <Route path="Application" element={<Application />} />
-            <Route path="student/register" element={<Subscribe />} />
-            <Route path="APIApproval" element={<APIApproval />} />
-            //KYC-Verification
-            <Route path="KYC-Verification" element={<KYCVerification />} />
-            //Report
-            <Route path="todayreport" element={<TodayReport />} />
-            <Route path="daywisereport" element={<DayWiseReport />} />
-            <Route path="ApprovedAPIReport" element={<ApprovedAPIReport />} />
-            <Route path="RechargeReport" element={<RechargeReport />} />
-            //Setting
-            <Route path="setting" element={<Setting/>} />
-            <Route path="changePassword" element={<ChangePass />} />
-            <Route path="Profile" element={<Profile />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,84 +1,11 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { Card, Row, Col } from "react-bootstrap";
-import { getApiServiceMenu } from "../../apiService/commonApi";
-import { useNavigate } from "react-router-dom";
-import { apiRequest } from "../../utils/apiRequest";
-import * as urls from "../../utils/url";
-import toastNotify from "../../utils/tostNotify";
-import { imgLogo } from "./iconImg";
+import React from 'react'
 
-const Dashboard: React.FC = () => {
-  const [updateSidebarMenu, setUpdateSidebarMenu] = useState<any>([]);
-  const navigator = useNavigate();
-
-  // Get Menu API service Sub Menu
-  const getApiServiceSubMenu = useCallback(async () => {
-    const result: any = await getApiServiceMenu();
-    const serviceMenus: any = [];
-
-    // Do seprate to service menus and store in serviceMenus variable
-    result?.map((items: any) => {
-      items?.details?.map((data: any) => {
-        serviceMenus?.push(data);
-      })
-    })
-
-    // ServiceMenus store in a state
-    setUpdateSidebarMenu(serviceMenus);
-  }, []);
-
-  useEffect(() => {
-    getApiServiceSubMenu();
-  }, []);
-
-  const getApiServiceInputs = async (data: any) => {
-    try {
-      const payload = {
-        apiCd: data.apiCd,
-      };
-      const config = {};
-      const result = await apiRequest("POST", urls.apiVerificationInput, payload, config);
-      if (result.statusCode === 0) {
-        // return result.data;
-        navigator("/dashboard/apiservice", { state: { data: result.data, serviceNm: data.serviceNm, details: data } })
-      } else {
-        result.message !== "No data found" &&
-          toastNotify(result.message, "error");
-      }
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
-
-
+function Dashboard() {
   return (
-    <>
-      <div>
-        <h6>API Service</h6>
-      </div>
-      <Card style={{ height: "calc(100vh - 230px)" }} className="overflow-auto">
-        <Card.Body>
-          <Row>
-            {updateSidebarMenu.map((menus: any) => (
-              <Col md={3}>
-                <Card onClick={() => getApiServiceInputs(menus)} className="p-3 details_Card">
-                  <Row>
-                    <Col md={1}>
-                      <img title="image" className="" style={{ height: "35px", width: "35px" }} src={imgLogo} />
-                    </Col>
-                    <Col className="ms-3">
-                      <h6 className="fw-semibold text-start mb-1">  {menus.serviceNm} </h6>
-                      <p className="text-sm api-description mb-0"> {menus.description}</p>
-                    </Col>
-                  </Row>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Card.Body>
-      </Card>
-    </>
-  );
-};
+    <div>
+      Dashboard
+    </div>
+  )
+}
 
-export default Dashboard;
+export default Dashboard
